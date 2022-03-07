20e77f761df9eb1477cbffa71edebf81a637ee5a
Faisal <m.faisal.s@outlook.com>
2022-03-07 10:59:28 +0530

    Update

--- 22. Binary Search/main.c
+++ 22. Binary Search/main.c
@@ -2,19 +2,19 @@
 
 int a[10], i, j, k_r, mid, temp;
 
-int bnry_ser(int lwr, int upr){
+int binarySearch(int lwr, int upr){
     if(upr >= lwr){
         mid = (lwr + upr)/2;
         if(a[mid] == k_r)
             return mid;
         else if(a[mid] < k_r)
-            return bnry_ser(mid+1, upr);
-        return bnry_ser(lwr, mid-1);
+            return binarySearch(mid+1, upr);
+        return binarySearch(lwr, mid-1);
     }
     return -1;
 }
 
-void bbl_srt(int n){
+void bubbleSort(int n){
     for(i=0; i<n; i++)
         for(j=0; j<n-1-i; j++)
             if(a[j] > a[j+1]){
@@ -34,11 +34,11 @@ void main(){
     }
     printf("Enter key element : ");
     scanf("%d", &k_r);
-    bbl_srt(n);
+    bubbleSort(n);
     printf("After sorting the elements in the array are\n");
     for(j=0; j<n; j++)
         printf("Value of a[%d] = %d\n", j, a[j]);
-    temp = bnry_ser(0, n-1);
+    temp = binarySearch(0, n-1);
     if(temp == -1)
         printf("The Key element %d is not found in the array\n", k_r);
     else
